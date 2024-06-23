bd094760670caf885e2e79a2ad3c64a50584cea5
Paulo de Tarso F. Machado <paulodetarsofm@yahoo.com.br>
2024-06-23 10:21:44 +0100

    Exercise: Part 2 - Course Information → Refactor the app

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,61 +1,57 @@
-const Header = (props) => {
+const Header = ({ course }) => {
   return (
-    <>
-      <h1>{props.course.name}</h1>
-    </>
+    <h1>{course}</h1>
   )
 }
 
-const Part = (props) => {
+const Part = ({ part }) => {
   return (
     <p>
-      {props.part.name} {props.part.exercises}
+      {part.name} {part.exercises}
     </p>
   )
 }
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </>
   )
 }
 
-const Total = (props) => {
-  const total = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
-
+const Total = ({ total }) => {
   return (
     <p>Number of exercises {total}</p>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
 
   return (
     <>
       <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Content parts={parts} />
+      <Total total={total} />
     </>
   )
 }
